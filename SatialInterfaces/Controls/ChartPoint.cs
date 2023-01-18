0f7a520c1e4b54322581e3a66ef33626a525e0f7
satial-interfaces <satial.interfaces@protonmail.com>
2023-01-18 08:52:55 +0100

    Moving to objects for x and y values

--- SatialInterfaces/Controls/ChartPoint.cs
+++ SatialInterfaces/Controls/ChartPoint.cs
@@ -11,9 +11,9 @@ public interface IChartPoint : IControl
 	/// <summary>Index property.</summary>
 	int Index { get; set; }
 	/// <summary>The X position.</summary>
-	double X { get; set; }
+	object? X { get; set; }
 	/// <summary>The Y position.</summary>
-	double Y { get; set; }
+	object? Y { get; set; }
 }
 
 /// <summary>This class represents a chart point.</summary>
@@ -21,22 +21,22 @@ public interface IChartPoint : IControl
 public class ChartPoint : Ellipse, IChartPoint
 {
 	/// <summary>X position property.</summary>
-	public static readonly DirectProperty<ChartPoint, double> XProperty = AvaloniaProperty.RegisterDirect<ChartPoint, double>(nameof(X), o => o.X, (o, v) => o.X = v);
+	public static readonly DirectProperty<ChartPoint, object?> XProperty = AvaloniaProperty.RegisterDirect<ChartPoint, object?>(nameof(X), o => o.X, (o, v) => o.X = v);
 	/// <summary>Y position property.</summary>
-	public static readonly DirectProperty<ChartPoint, double> YProperty = AvaloniaProperty.RegisterDirect<ChartPoint, double>(nameof(Y), o => o.Y, (o, v) => o.Y = v);
+	public static readonly DirectProperty<ChartPoint, object?> YProperty = AvaloniaProperty.RegisterDirect<ChartPoint, object?>(nameof(Y), o => o.Y, (o, v) => o.Y = v);
 	/// <summary>Is selected property.</summary>
 	public bool IsSelected { get => isSelected; set { isSelected = value; PseudoClasses.Set(":selected", isSelected); } }
 	/// <inheritdoc />
 	public int Index { get; set; }
 	/// <inheritdoc />
-	public double X { get => x; set => SetAndRaise(XProperty, ref x, value); }
+	public object? X { get => x; set => SetAndRaise(XProperty, ref x, value); }
 	/// <inheritdoc />
-	public double Y { get => y; set => SetAndRaise(YProperty, ref y, value); }
+	public object? Y { get => y; set => SetAndRaise(YProperty, ref y, value); }
 
 	/// <summary>Is selected.</summary>
 	bool isSelected;
 	/// <summary>X position.</summary>
-	double x;
+	object? x;
 	/// <summary>Y position.</summary>
-	double y;
+	object? y;
 }
